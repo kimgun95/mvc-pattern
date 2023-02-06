1c4eb029eaefb8d62c9917e966228c41c0eacd70
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-02-06 20:08:50 +0900

    initialize project

---
+++ src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -0,0 +1,22 @@
+package org.example.mvc;
+
+import org.example.mvc.controller.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestMappingHandlerMapping implements HandlerMapping {
+    // [key] : /users  , [value] : UserController
+    private Map<HandlerKey, Controller> mapping = new HashMap<>();
+
+    void init() {
+//        mapping.put(new HandlerKey(RequestMethod.GET, "/"), new HomeController());
+        mapping.put(new HandlerKey(RequestMethod.GET, "/users"), new UserListController());
+        mapping.put(new HandlerKey(RequestMethod.POST, "/users"), new UserCreateController());
+        mapping.put(new HandlerKey(RequestMethod.GET, "/user/form"), new ForwardController("/user/form"));
+    }
+
+    public Controller findHandler(HandlerKey handlerKey) {
+        return mapping.get(handlerKey);
+    }
+}
